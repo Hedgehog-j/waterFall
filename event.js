52a5d212076ef636ab4db190a6dd0e726262d7d3
Hedgehog-j <513412508@qq.com>
2017-09-19 15:58:12 +0800

    Create event.js

---
+++ event.js
@@ -0,0 +1,40 @@
+var EventUtil={
+	addHandler:function(element,type,handler){
+		if(element.addEventListener){
+			element.addEventListener(type,handler,false);
+		}else if(element.attachEvent){
+			element.attachEvent("on"+type,handler);
+		}else{
+			element["on"+type]=handler;
+		}
+	},
+	removeHandler:function(element,type,handler){
+		if(element.removeEventListener){
+			element.removeEventListener(type,handler,false);
+		}else if(element.detachEvent){
+			element.detachEvent("on"+type,handler);
+		}else{
+			element["on"+type]=null;
+		}
+	},
+	getEvent:function(event){
+		return event? event:window.evnet;
+	},
+	getTarget:function(event){
+		return event.target||event.srcElment;
+	},
+	preventDefault:function(event){
+		if(event.preventDefault){
+			event.preventDefault();
+		}else{
+			event.returnValue=false;
+		}
+	},
+	stopPropagation:function(event){
+		if(event.stopPropagation){
+			evetn.stopPropagation();
+		}else{
+			event.cancelBubble=true;
+		}
+	}
+};
